57a159e698eb6c9dc10204ec5961223c62965c60
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-11-28 13:35:11 -0500

    Update Japo component layout and styling; add text overlay effect in CSS ## Repsonsive 1536x1080 y 1920x1080 Hecho

--- src/Pages/Japo.tsx
+++ src/Pages/Japo.tsx
@@ -18,30 +18,30 @@ export function Japo() {
         "CaruselJapo5.jpg", "CaruselJapo6.jpg",
     ]
     const caruselItems = CAROUSELIMGs.map((src, i) => {
-        return <img src={`/Media/${src}`} alt={src} width={200} key={i} className='rounded-2xl snap-center snap-always w-[200px] h-[200px]'/>
+        return <img src={`/Media/${src}`} alt={src} width={200} key={i} className='rounded-[125px] snap-center snap-always w-[200px] h-[200px] object-contain'/>
     })
     
-    setInterval(() => {
-        moveCarusel();
-    }, 6000);
+    // const caruselInterval = setInterval(() => {
+    //     moveCarusel();
+    // }, 6000);
     
 
     return (
-        <article className='relative'>
-            <div className='font-BebasNeue text-9xl w-1/2 text-center'>
-                <h2 className={`bg-[url(Media/fondoJapo2.jpg)] bg-contain bg-clip-text text-transparent backdrop-blur-xs p-4`}>
+        <article className='grid grid-cols-3 grid-rows-4 relative h-screen'>
+            <div className='font-BebasNeue text-9xl w-1/2 content-start justify-items-center justify-self-center col-start-1 col-span-2 row-start-2 row-end-1'>
+                <h2 className={`bg-[url(Media/fondoJapo2.jpg)] bg-contain bg-clip-text text-transparent backdrop-blur-xs px-8 absolute`}>
                     <i>JAPON</i>
                 </h2>
-                <h2 className='absolute top-6 left-[18.5vw] z-[-1]'>
+                <h2 className='absolute z-[-1] translate-2'>
                     <i>JAPON</i>
                 </h2>
             </div>
-            <section className='w-3/4 z-[-1] h-[220px] bg-[#c198c045] mx-12 mt-8 rounded-2xl flex flex-row overflow-hidden'>
-                <div id='carusel' className='snap-x snap-mandatory flex flex-col gap-16 p-2 flex-wrap overflow-hidden'>
+            <section className='w-3/4 z-[-1] h-[180px] bg-[#c198c045] min-[1550px]:mb-0 mb-6 rounded-2xl flex flex-row overflow-hidden col-start-1 col-span-3 row-start-1 row-span-2  min-[1550px]:self-center self-end justify-self-center p-2 '>
+                <div id='carusel' className='snap-x snap-mandatory flex flex-col gap-x-16 flex-wrap overflow-hidden rounded-2xl'>
                     {caruselItems}
                 </div>
             </section>
-            <section className='max-w-lg text-justify leading-relaxed mb-8 font-BilboSwashCaps absolute left-[12vw] mt-8 text-2xl flex flex-col gap-4 text-[#2D2363] backdrop-blur-lg [text-shadow:0_4px_4px_#fff]'>
+            <section className='max-w-lg text-justify leading-relaxed mb-8 font-BilboSwashCaps mt-8 min-[1550px]:text-2xl text-lg flex flex-col gap-4 text-[#2D2363] backdrop-blur-lg [text-shadow:0_4px_4px_#fff] col-start-1 col-span-2 row-start-3 min-[1550px]:row-start-2 min-[1550px]:row-end-5  self-center p-4 justify-self-center'>
                 <p>
                     Descubre Japón, un destino donde la tradición y la modernidad conviven en perfecta armonía. Recorre templos milenarios en Kioto.
                 </p>
@@ -55,8 +55,16 @@ export function Japo() {
                     Japón te espera con su hospitalidad incomparable, paisajes sorprendentes y experiencias culturales que recordarás toda la vida. ¡Atrévete a vivir la magia del País del Sol Naciente!
                 </p>
             </section>
-            <figure className='justify-self-end absolute top-0'>
-                <img src={FondoSvg} alt="Imagen de el Monte Fuji" className='h-[140vh]'/>
+            <figure className='justify-self-end h-full w-full col-start-2 col-span-3 row-start-1 -row-end-1 relative'>
+                <div>
+                    {/* fondo blur */}
+                    <img
+                        src={FondoSvg}
+                        className="absolute inset-0 w-full object-cover blur-lg scale-100 z-[-1]"
+                    />
+                    {/* imagen normal */}
+                    <img src={FondoSvg} alt="Imagen de el Monte Fuji" className='w-full '/>
+                </div>
             </figure>
         </article>
     );
